0f967f3c584ad7c923f681353137c8fe41d8fe89
Emir Aydın <emir.aydin2002@gmail.com>
2024-12-07 21:10:31 +0300

    Parser1

---
+++ src/main/java/project/ClassroomRead.java
@@ -0,0 +1,16 @@
+package project;
+
+class ClassroomRead {
+    String classroomName;
+    int capacity;
+
+    public ClassroomRead(String classroomName, int capacity) {
+        this.classroomName = classroomName;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return classroomName + " with capacity " + capacity;
+    }
+}
